c3cd2da29bd527faf4665a1648ca5154a81d6b08
shixiaofei <shixiaofei@neusoft.com>
2013-05-29 14:31:43 -0700

    THOR-3145: Add the images in LK for charging/low battery/device too hot.
    [Issues]
    Need different images for different situation.
    
    [Fix]
    Add the images in the LK.
    
    [Test]
    Tested on Thor EVT1.
    
    Change-Id: Ie53c5187524571a6987508f416c669b9f15d4f56

--- target/thor/image/rules.mk
+++ target/thor/image/rules.mk
@@ -1,7 +1,8 @@
 LOCAL_DIR := $(GET_LOCAL_DIR)
 
 OBJS += \
-	$(LOCAL_DIR)/image_thor.o
-
-
+	$(LOCAL_DIR)/image_thor.o \
+	$(LOCAL_DIR)/image_charge.o \
+	$(LOCAL_DIR)/image_low.o \
+	$(LOCAL_DIR)/image_hot.o \
 
