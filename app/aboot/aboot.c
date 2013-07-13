d562045f0c74ecd4f0a616b4850481c84d816bfb
Deepa Dinamani <deepad@codeaurora.org>
2013-07-12 17:28:29 -0700

    app: aboot: Add checks for overlapping aboot addresses with device read buffers.
    CRs-Fixed: 491421
    
    Change-Id: Id2ca648ff9272994b24f6743a25c7c31e4e5d075

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -634,7 +634,7 @@ int boot_linux_from_mmc(void)
 		dt_actual = ROUND_TO_PAGE(hdr->dt_size, page_mask);
 		imagesize_actual = (page_size + kernel_actual + ramdisk_actual + dt_actual);
 
-		if (check_aboot_addr_range_overlap(hdr->tags_addr, hdr->dt_size))
+		if (check_aboot_addr_range_overlap(hdr->tags_addr, dt_actual))
 		{
 			dprintf(CRITICAL, "Device tree addresses overlap with aboot addresses.\n");
 			return -1;
@@ -646,6 +646,12 @@ int boot_linux_from_mmc(void)
 
 		dprintf(INFO, "Loading boot image (%d): start\n", imagesize_actual);
 
+		if (check_aboot_addr_range_overlap(image_addr, imagesize_actual))
+		{
+			dprintf(CRITICAL, "Boot image buffer address overlaps with aboot addresses.\n");
+			return -1;
+		}
+
 		/* Read image without signature */
 		if (mmc_read(ptn + offset, (void *)image_addr, imagesize_actual))
 		{
@@ -656,6 +662,13 @@ int boot_linux_from_mmc(void)
 		dprintf(INFO, "Loading boot image (%d): done\n", imagesize_actual);
 
 		offset = imagesize_actual;
+
+		if (check_aboot_addr_range_overlap(image_addr + offset, page_size))
+		{
+			dprintf(CRITICAL, "Signature read buffer address overlaps with aboot addresses.\n");
+			return -1;
+		}
+
 		/* Read signature */
 		if(mmc_read(ptn + offset, (void *)(image_addr + offset), page_size))
 		{
