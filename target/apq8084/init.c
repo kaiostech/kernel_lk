25f686e0c88dc6e4088d8b61ce4aa58d3cb3474f
Dhaval Patel <pdhaval@codeaurora.org>
2013-10-18 19:06:05 -0700

    target: apq8084: Add display support for apq8084 target
    Add display support in lk with continuous splash screen
    for APQ8084 target with GCDB, dynamic panel selection
    and auto PLL.
    
    Change-Id: I02812f28bc700603e0abf737ce06a7316094a5fd

--- target/apq8084/init.c
+++ target/apq8084/init.c
@@ -262,6 +262,23 @@ void target_baseband_detect(struct board_data *board)
 	}
 }
 
+/* Returns 1 if target supports continuous splash screen. */
+int target_cont_splash_screen()
+{
+	switch(board_hardware_id())
+	{
+		case HW_PLATFORM_SURF:
+		case HW_PLATFORM_MTP:
+		case HW_PLATFORM_FLUID:
+			dprintf(INFO, "Target_cont_splash=1\n");
+			return 1;
+			break;
+		default:
+			dprintf(SPEW, "Target_cont_splash=0\n");
+			return 0;
+	}
+}
+
 unsigned target_baseband()
 {
 	return board_baseband();
