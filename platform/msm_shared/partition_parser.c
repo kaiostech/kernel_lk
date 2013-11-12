6657b4612327a8d5386150309b108a9727326757
Channagoud Kadabi <ckadabi@codeaurora.org>
2013-11-12 13:49:41 -0800

    platform: msm_shared: Fix max number of partition table entries
    Max number of partition table entries is defined to 32, but the
    gpt/mbr spec does not have any hard limit on the max entries.
    Increase the max value to a safer number to 128.
    
    CRs-Fixed: 551703
    Change-Id: I93c9e98f3efb8fa85f99225e944ae86f834f7b5a

--- platform/msm_shared/partition_parser.c
+++ platform/msm_shared/partition_parser.c
@@ -58,13 +58,17 @@ char *vfat_partitions[] = { "modem", "mdm", "NONE" };
 unsigned int ext3_count = 0;
 unsigned int vfat_count = 0;
 
-struct partition_entry partition_entries[NUM_PARTITIONS];
+struct partition_entry *partition_entries;
 static unsigned gpt_partitions_exist = 0;
 unsigned partition_count = 0;
 
 unsigned int partition_read_table()
 {
 	unsigned int ret;
+
+	/* Allocate partition entries array */
+	partition_entries = (struct partition_entry *) calloc(NUM_PARTITIONS, sizeof(struct partition_entry));
+	ASSERT(partition_entries);
 
 	/* Read MBR of the card */
 	ret = mmc_boot_read_mbr();
