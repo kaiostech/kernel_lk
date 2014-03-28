6caa27b284d669c91b7a06b4d6e4434f1af86093
Sundarajan Srinivasan <sundaraj@codeaurora.org>
2014-03-28 16:45:21 -0700

    platform: msm8226: Modify MSM_SHARED_BASE based on platform subtype.
    Add the MSM_SHARED_BASE address for the new QVGA platform subtype.
    Also implement the api which returns MSM_SHARED_BASE based on platform
    subtype.
    
    Change-Id: I89cbc6415c030123494fce703a1fa6474ea441ab

--- platform/msm8226/platform.c
+++ platform/msm8226/platform.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2013, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -140,4 +140,9 @@ void platform_init_mmu_mappings(void)
 								mmu_section_table[i].flags);
 		}
 	}
+}
+
+uint32_t platform_get_smem_base_addr()
+{
+	return (uint32_t)(readl(MSM_DYNAMIC_SHARED_BASE));
 }
