7e9ce93dad4450cda462679359f0699c2a599aa3
Dhaval Patel <pdhaval@codeaurora.org>
2014-04-16 10:54:32 -0700

    target: Remove pll configuration for DSI1 PHY
    Remove pll configuration for dsi1_phy because
    pclk0 and pclk1 are sourced from dsi0 PHY in all
    dual dsi cases.
    
    CRs-fixed: 642087
    Change-Id: I180320e4378b6c094cbe952a7dcfeffedd27074f

--- target/msm8974/target_display.c
+++ target/msm8974/target_display.c
@@ -221,11 +221,6 @@ int target_panel_clock(uint8_t enable, struct msm_panel_info *pinfo)
 		mdp_clock_init();
 		mdss_dsi_auto_pll_config(MIPI_DSI0_BASE, pll_data);
 		dsi_pll_enable_seq(MIPI_DSI0_BASE);
-		if (panel.panel_info.mipi.dual_dsi &&
-				!(panel.panel_info.mipi.broadcast)) {
-			mdss_dsi_auto_pll_config(MIPI_DSI1_BASE, pll_data);
-			dsi_pll_enable_seq(MIPI_DSI1_BASE);
-		}
 		mmss_clock_auto_pll_init(DSI0_PHY_PLL_OUT, dual_dsi,
 					pll_data->pclk_m,
 					pll_data->pclk_n,
