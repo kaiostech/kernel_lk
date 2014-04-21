3450f4662b8c7d0a9c052fedf7ea14eb87d0bba0
Sundarajan Srinivasan <sundaraj@codeaurora.org>
2014-04-21 14:14:30 -0700

    platform: msm_shared: Size of decrypted signature should be equal to size of digest.
    For a successful boot image authentication, the RSA_public_decrypt() api returns
    the size of the digest otherwise its a failure. This check avoids vulnerability due to
    trailing data placed at the end of digest.
    
    CRs-Fixed: 646385
    
    Conflicts:
            platform/msm_shared/image_verify.c
    
    Change-Id: I32c2d97d5c308d33146c509347915ca65fdd11ac

--- platform/msm_shared/image_verify.c
+++ platform/msm_shared/image_verify.c
@@ -98,9 +98,28 @@ image_verify(unsigned char *image_ptr,
 		goto cleanup;
 	}
 
+	/*
+	 * Calculate hash of image and save calculated hash on TZ.
+	 */
+	hash_size =
+	    (hash_type == CRYPTO_AUTH_ALG_SHA256) ? SHA256_SIZE : SHA1_SIZE;
+	hash_find(image_ptr, image_size, (unsigned char *)&digest, hash_type);
+#ifdef TZ_SAVE_KERNEL_HASH
+	if (hash_type == CRYPTO_AUTH_ALG_SHA256) {
+		save_kernel_hash_cmd(digest);
+		dprintf(INFO, "Image hash saved.\n");
+	} else
+		dprintf(INFO, "image_verify: hash is not SHA-256.\n");
+#endif
+
+	/*
+	 * Decrypt the pre-calculated expected image hash.
+	 * Return value, ret should be equal to hash_size. Otherwise it means a failure. With this check
+	 * we avoid a potential vulnerability due to trailing data placed at the end of digest.
+	 */
 	ret = image_decrypt_signature(signature_ptr, plain_text);
-	if (ret == -1) {
-		dprintf(CRITICAL, "ERROR: Image Invalid! Decryption failed!\n");
+	if (ret != hash_size) {
+		dprintf(CRITICAL, "ERROR: Image Invalid! signature check failed! ret %d\n", ret);
 		goto cleanup;
 	}
 
