msm8916-mainline/lk2nd

platform: msm_shared: delay DSI PHY lane contention detection
The DSI PHY lane contention detection should only be enabled
after data lanes are enabled.

Change-Id: I5b59e498c5f40b33f370ce1ff0d63ef9efb2c924

platform/msm_shared/include/mipi_dsi.h
@@ -219,6 +219,8 @@ int mipi_config(struct msm_fb_panel_data *panel);
 int mdss_dsi_config(struct msm_fb_panel_data *panel);
 int mdss_dsi_phy_init(struct mipi_dsi_panel_config *,
 		uint32_t ctl_base, uint32_t phy_base);
+void mdss_dsi_phy_contention_detection(struct mipi_dsi_panel_config *,
+				uint32_t phy_base);
 
 int mdss_dsi_video_mode_config(uint16_t disp_width,
 	uint16_t disp_height,

platform/msm_shared/mipi_dsi.c
@@ -684,6 +684,8 @@ int mdss_dsi_config(struct msm_fb_panel_data *panel)
 		goto error;
 	}
 
+	mdss_dsi_phy_contention_detection(&mipi_pinfo, DSI0_PHY_BASE);
+
 	if (panel->pre_init_func) {
 		ret = panel->pre_init_func();
 		if (ret) {

platform/msm_shared/mipi_dsi_phy.c
@@ -310,8 +310,6 @@ int mdss_dsi_phy_init(struct mipi_dsi_panel_config *pinfo,
 	/* MMSS_DSI_0_PHY_DSIPHY_CTRL_0 */
 	writel(0x5f, phy_base + 0x0170);
 
-	/* Strength ctrl 1 */
-	writel(pd->strength[1], phy_base + 0x0188);
 	dmb();
 	/* 4 lanes + clk lane configuration */
 	/* lane config n * (0 - 4) & DataPath setup */
@@ -349,3 +347,18 @@ int mdss_dsi_phy_init(struct mipi_dsi_panel_config *pinfo,
 	dmb();
 
 }
+
+void mdss_dsi_phy_contention_detection(
+		struct mipi_dsi_panel_config *pinfo,
+		uint32_t phy_base)
+{
+        struct mdss_dsi_phy_ctrl *pd;
+
+        if (mdp_get_revision() == MDP_REV_304)
+                return;
+
+        pd = (pinfo->mdss_dsi_phy_config);
+	writel(pd->strength[1], phy_base + 0x0188);
+	dmb();
+}
+

kaiostech/kernel_lk

platform/msm_shared/include/mipi_dsi.h
@@ -972,6 +972,8 @@ enum {		/* mipi dsi panel */
 
 int mipi_config(struct msm_fb_panel_data *panel);
 int mdss_dsi_config(struct msm_fb_panel_data *panel);
+void mdss_dsi_phy_contention_detection(struct mipi_dsi_panel_config *,
+				uint32_t ctrl_base);
 
 int mdss_dsi_video_mode_config(uint16_t disp_width,
 	uint16_t disp_height,

platform/msm_shared/mipi_dsi.c
@@ -1035,6 +1035,8 @@ int mdss_dsi_config(struct msm_fb_panel_data *panel)
 		goto error;
 	}
 
+	mdss_dsi_phy_contention_detection(&mipi_pinfo, MIPI_DSI0_BASE);
+
 	if (panel->pre_init_func) {
 		ret = panel->pre_init_func();
 		if (ret) {

platform/msm_shared/mipi_dsi_phy.c
@@ -320,8 +320,6 @@ int mdss_dsi_phy_init(struct mipi_dsi_panel_config *pinfo, uint32_t ctl_base)
 	/* MMSS_DSI_0_PHY_DSIPHY_CTRL_0 */
 	writel(0x5f, ctl_base + 0x0470);
 
-	/* Strength ctrl 1 */
-	writel(pd->strength[1], ctl_base + 0x0488);
 	dmb();
 	/* 4 lanes + clk lane configuration */
 	/* lane config n * (0 - 4) & DataPath setup */
@@ -358,3 +356,18 @@ int mdss_dsi_phy_init(struct mipi_dsi_panel_config *pinfo, uint32_t ctl_base)
 	dmb();
 
 }
+
+void mdss_dsi_phy_contention_detection(
+		struct mipi_dsi_panel_config *pinfo,
+		uint32_t ctrl_base)
+{
+        struct mdss_dsi_phy_ctrl *pd;
+
+        if (mdp_get_revision() == MDP_REV_304)
+                return;
+
+        pd = (pinfo->mdss_dsi_phy_config);
+	writel(pd->strength[1], ctrl_base + 0x0488);
+	dmb();
+}
+