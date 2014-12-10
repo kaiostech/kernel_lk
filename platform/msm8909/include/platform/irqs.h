02b9fa66a250ddcef849be6b5f3b37187a109b8e
Unnati Gandhi <ugandhi@codeaurora.org>
2014-12-10 15:39:12 +0530

    platform: msm8909: Add changes to support I2C QUP
    Add changes to support I2C QUP for msm8909.
    
    Change-Id: I9ec51c88500166b42a65d1876f6d769ad25fa89a

--- platform/msm8909/include/platform/irqs.h
+++ platform/msm8909/include/platform/irqs.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014, 2017, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -64,4 +64,5 @@
 #define NR_IRQS                                (NR_MSM_IRQS + NR_GPIO_IRQS + \
                                                NR_BOARD_IRQS)
 
+#define BLSP_QUP_IRQ(blsp_id, qup_id)          (GIC_SPI_START + 95 + qup_id)
 #endif /* __IRQS_MSM8909_H */
