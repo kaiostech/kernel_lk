3e9eb1c1add7005c24244c4e5584868f4a17f294
Aparna Mallavarapu <aparnam@codeaurora.org>
2014-12-24 12:54:35 +0530

    platform: msm_shared: Make the SSD Image decrypt SCM call atomic
    Make an atomic SCM call for SSD Image decryption so that IRQs are disabled
    until the scm call is returned.
    
    Conflicts:
    	platform/msm_shared/scm.c
    
    Change-Id: I62502a6af2afb384beae2b8258f3c1e34ef30541

--- platform/msm_shared/scm.c
+++ platform/msm_shared/scm.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2011-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2011-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -425,6 +425,7 @@ static int ssd_image_is_encrypted(uint32_t ** img_ptr, uint32_t * img_len_ptr, u
 			scm_arg.x1 = MAKE_SCM_ARGS(0x2,SMC_PARAM_TYPE_VALUE,SMC_PARAM_TYPE_BUFFER_READWRITE);
 			scm_arg.x2 = parse_req.md_len;
 			scm_arg.x3 = parse_req.md;
+			scm_arg.atomic = true;
 
 			ret = scm_call2(&scm_arg, &scm_ret);
 			parse_rsp.status = scm_ret.x1;
