7f1d85389c3fb3186ca467d197dfb1056caeb54c
Unnati Gandhi <ugandhi@codeaurora.org>
2015-02-05 15:48:18 +0530

    platform: msm_shared: Add macro corresponding to 163MHZ freq
    Add macro corresponding to 163MHZ frequency to be used for
    SDC.
    
    Change-Id: Ia345634b7be3302db0f09162665395e07f663ba6

--- platform/msm_shared/include/mmc_sdhci.h
+++ platform/msm_shared/include/mmc_sdhci.h
@@ -150,6 +150,7 @@
 #define MMC_CLK_48MHZ                             48000000
 #define MMC_CLK_50MHZ                             49152000
 #define MMC_CLK_96MHZ                             96000000
+#define MMC_CLK_163MHZ                            163400000
 #define MMC_CLK_177MHZ                            177770000
 #define MMC_CLK_200MHZ                            200000000
 #define MMC_CLK_192MHZ                            192000000
