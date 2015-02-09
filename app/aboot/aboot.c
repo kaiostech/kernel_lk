4e59a5091463a4671e25a1846a8a80806e1e4deb
Reut Zysman <rzysman@codeaurora.org>
2015-02-09 13:47:27 +0200

    aboot: fwlock: Add support for recovery PIN
    Add support for recovery PIN.
    
    Change-Id: I3023118483f5c053b7fd21f8188a5e359d5cb50e

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -1717,7 +1717,7 @@ void cmd_boot(const char *arg, void *data, unsigned sz)
 #ifdef MDTP_SUPPORT
 	/* Go through Firmware Lock verification before continue with boot process */
 	mdtp_fwlock_verify_lock();
-	fbcon_clear();
+	display_image_on_screen();
 #endif /* MDTP_SUPPORT */
 
 #if VERIFIED_BOOT
@@ -2345,7 +2345,7 @@ void cmd_continue(const char *arg, void *data, unsigned sz)
 #ifdef MDTP_SUPPORT
 	/* Go through Firmware Lock verification before continue with boot process */
 	mdtp_fwlock_verify_lock();
-	fbcon_clear();
+	display_image_on_screen();
 #endif /* MDTP_SUPPORT */
 
 	if (target_is_emmc_boot())
@@ -2823,7 +2823,7 @@ normal_boot:
 #ifdef MDTP_SUPPORT
 			/* Go through Firmware Lock verification before continue with boot process */
 			mdtp_fwlock_verify_lock();
-			fbcon_clear();
+			display_image_on_screen();
 #endif /* MDTP_SUPPORT */
 
 			boot_linux_from_mmc();
