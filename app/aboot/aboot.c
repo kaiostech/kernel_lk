84da99522fc76a9399ae64b1b72c66d1c3495815
Sridhar Ancha <sancha@codeaurora.org>
2015-03-04 15:15:25 +0530

    app: aboot: Update the flash ptable with CNSS partition
    Add non-hlos CNSS partition in the flash ptable.
    
    Change-Id: I91cd4354feae53e651629a4c27a487b0d9201d18

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -2,7 +2,7 @@
  * Copyright (c) 2009, Google Inc.
  * All rights reserved.
  *
- * Copyright (c) 2009-2011, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2009-2011,2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
@@ -1281,6 +1281,7 @@ void cmd_flash(const char *arg, void *data, unsigned sz)
 	if (!strcmp(ptn->name, "system")
 		|| !strcmp(ptn->name, "userdata")
 		|| !strcmp(ptn->name, "persist")
+		|| !strcmp(ptn->name, "cnss")
 		|| !strcmp(ptn->name, "recoveryfs")) {
 		if (flash_ecc_bch_enabled())
 			/* Spare data bytes for 8 bit ECC increased by 4 */
