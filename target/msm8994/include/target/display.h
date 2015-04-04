20c714ae5bdcef2cd8e60c02e45ccfc0d13721c4
Ajay Singh Parmar <aparmar@codeaurora.org>
2015-04-03 19:06:03 -0700

    target: msm8994: hdmi: change fb address
    Use a unique frame buffer address for external display for
    continuous splash screen.
    
    Change-Id: I5a364b017c127b1c4e2efaeb74c5f3fc9587d6d8

--- target/msm8994/include/target/display.h
+++ target/msm8994/include/target/display.h
@@ -95,7 +95,7 @@ static const uint32_t panel_physical_ctrl[] = {
 #define DISPLAY_CMDLINE_PREFIX " mdss_mdp.panel="
 
 #define MIPI_FB_ADDR  0x03400000
-#define HDMI_FB_ADDR  0x03400000
+#define HDMI_FB_ADDR  0x0EC00000
 
 #define MIPI_HSYNC_PULSE_WIDTH       16
 #define MIPI_HSYNC_BACK_PORCH_DCLK   32
