fe338704cabc394a35bea4c73273cd6d963eda8d
Atul <araut@codeaurora.org>
2015-04-17 14:48:48 -0700

    platform: msm8960: set boot_done bit
    The boot_done flag if not set, causes the PMIC to reset when 90 seconds
    have elapsed after power on. This reset feature was introduced to ensure
    that the battery was strong enough to bootup the device, in case it was
    not the reset will charge the battery before trying the next bootup.
    However on Automotive project the PMIC runs on a battery less configuration
    so it is safe to set the boot_done bit in the appsbl to prevent the 90
    second reset.
    
    Change-Id: I122a1c516928b4a461e70f1e2d5a6258af416458

--- platform/msm8960/platform.c
+++ platform/msm8960/platform.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2014, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2011-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -110,6 +110,8 @@ void platform_uninit(void)
 		gpio_set(84, 1);
 	}
 	platform_uninit_timer();
+	/* Set the BOOT_DONE flag in PM8921 */
+	pm8921_boot_done();
 }
 
 /* Setup memory for this platform */
