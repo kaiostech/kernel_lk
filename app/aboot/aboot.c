f7a81d560d0ce18c361e9ee5657ccab862edfe69
Michael Vines <mvines@codeaurora.org>
2015-04-16 17:39:23 -0700

    app: aboot: add "androidboot.emmc=false" to kernel cmdline when not emmc
    Change-Id: If0ed9125ac491a3ec8aab8bf5fa09e9678dad64e

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -129,6 +129,7 @@ struct fastboot_cmd_desc {
 static const char *emmc_cmdline = " androidboot.bootdevice=";
 #else
 static const char *emmc_cmdline = " androidboot.emmc=true";
+static const char *notemmc_cmdline = " androidboot.emmc=false";
 #endif
 static const char *usb_sn_cmdline = " androidboot.serialno=";
 static const char *androidboot_mode = " androidboot.mode=";
@@ -255,6 +256,8 @@ unsigned char *update_cmdline(const char * cmdline)
 		platform_boot_dev_cmdline(boot_dev_buf);
 		cmdline_len += strlen(boot_dev_buf);
 #endif
+	} else {
+		cmdline_len += strlen(notemmc_cmdline);
 	}
 
 	cmdline_len += strlen(usb_sn_cmdline);
@@ -373,6 +376,11 @@ unsigned char *update_cmdline(const char * cmdline)
 			if (have_cmdline) --dst;
 			while ((*dst++ = *src++));
 #endif
+		} else {
+			src = notemmc_cmdline;
+			if (have_cmdline) --dst;
+			have_cmdline = 1;
+			while ((*dst++ = *src++));
 		}
 
 		src = usb_sn_cmdline;
