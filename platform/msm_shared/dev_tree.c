662a7bc1637c511087a0637fa2d48baf453e84cd
Sridhar Parasuram <sridhar@codeaurora.org>
2015-06-22 13:50:15 -0700

    platform: msm_shared: Remove explicit deference to model variable
    Remove explicit deference to model variable
    
    Change-Id: I2bd8e0b4b9d0919bf8d9cb15caad22e835927789

--- platform/msm_shared/dev_tree.c
+++ platform/msm_shared/dev_tree.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2012-2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2012-2015, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -92,7 +92,7 @@ static uint32_t dev_tree_compatible(void *dtb)
 		ASSERT(model);
 		strlcpy(model, prop, len);
 	} else {
-		model[0] = '\0';
+		dprintf(INFO, "model does not exist in device tree\n");
 	}
 
 	/* Find the board-id prop from DTB , if board-id is present then
