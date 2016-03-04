340fbd32c0604f0c790835c68ff0564437efe169
Gurjant Kalsi <me@gurjantkalsi.com>
2016-03-04 13:05:54 -0800

    [squash] Remove unused files, format main app.

--- app/moot/moot.c
+++ app/moot/moot.c
@@ -32,34 +32,35 @@
 #include <trace.h>
 
 
-static void moot_init(const struct app_descriptor *app) {
-	// Initialize our boot subsystems.
-	init_usb_boot();
+static void moot_init(const struct app_descriptor *app)
+{
+    // Initialize our boot subsystems.
+    init_usb_boot();
 
 }
 
 static void moot_entry(const struct app_descriptor *app, void *args)
 {
-	do {
-		// Wait a few seconds for the host to try to talk to us over USB.
-		if (attempt_usb_boot())
-			break;
+    do {
+        // Wait a few seconds for the host to try to talk to us over USB.
+        if (attempt_usb_boot())
+            break;
 
-		// // Check the SPIFlash for an upgrade image.
-		// if (attempt_spifs_upgrade())
-		// 	break;
+        // // Check the SPIFlash for an upgrade image.
+        // if (attempt_spifs_upgrade())
+        //  break;
 
-	} while (false);
+    } while (false);
 
 
-	// Ensure the integrity of the system image.
-	// verify_system_image();
+    // Ensure the integrity of the system image.
+    // verify_system_image();
 
-	// Boot the main system image.
-	// do_boot();
+    // Boot the main system image.
+    // do_boot();
 }
 
 APP_START(moot)
-	.init = moot_init,
-	.entry = moot_entry,
-APP_END
+.init = moot_init,
+ .entry = moot_entry,
+  APP_END
