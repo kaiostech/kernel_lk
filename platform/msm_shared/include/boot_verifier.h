3c98195df273cbba5250c18f0318fa05b42e5860
P.V. Phani Kumar <pvphani@codeaurora.org>
2016-03-09 19:43:24 +0530

    platform: msm_shared: Remove static keyword for read_der_message_length()
    Enabling read_der_message_length() to be called outside of boot_verifier.c
    
    Change-Id: Ic0483232faefdf79092739429524a830881fdf85

--- platform/msm_shared/include/boot_verifier.h
+++ platform/msm_shared/include/boot_verifier.h
@@ -160,4 +160,5 @@ bool boot_verify_flash_allowed(const char * entry);
 bool boot_verify_compare_sha256(unsigned char *image_ptr,
 		unsigned int image_size, unsigned char *signature_ptr, RSA *rsa);
 KEYSTORE *boot_gerity_get_oem_keystore();
+uint32_t read_der_message_length(unsigned char* input, unsigned sz);
 #endif
