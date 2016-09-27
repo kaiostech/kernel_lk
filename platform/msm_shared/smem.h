486c695d50ded31f519074d69a5f6ec413dfc35a
Parth Dixit <parthd@codeaurora.org>
2016-09-27 12:29:30 +0530

    platform: msm_shared: add 8909 SWOC
    SWOC P4 will have round display. Add support for SOWC P4 subtype.
    
    Change-Id: Iee4a844d15dca4fe2aade39969ff7009dd68ba49

--- platform/msm_shared/smem.h
+++ platform/msm_shared/smem.h
@@ -2,7 +2,7 @@
  * Copyright (c) 2009, Google Inc.
  * All rights reserved.
  *
- * Copyright (c) 2009-2015, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2009-2016, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -435,6 +435,7 @@ enum platform_subtype {
 	HW_PLATFORM_SUBTYPE_DSDA2 = 8,
 	HW_PLATFORM_SUBTYPE_SGLTE2 = 9,
 	HW_PLATFORM_SUBTYPE_SWOC_WEAR = 9,
+	HW_PLATFORM_SUBTYPE_SWOC_TP_CIRC = 12,
 	HW_PLATFORM_SUBTYPE_32BITS = 0x7FFFFFFF
 };
 
