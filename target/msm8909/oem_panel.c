8487373fb5a658810ae3e0a771466083610fd4d4
raghavendra ambadas <rambad@codeaurora.org>
2016-12-14 12:30:20 +0530

    dev: gcdb: add HX8379C HVGA panel header
    Update header file with HX8379C HVGA panel support,
    this panel is used on MSM8905 skue target.
    
    Change-Id: Icd0482926a8bbf03c8b09a6a0d3fc4801193efe5
    Signed-off-by: Raghavendra Ambadas <rambad@codeaurora.org>

--- target/msm8909/oem_panel.c
+++ target/msm8909/oem_panel.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014-2016, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014-2017, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -45,6 +45,7 @@
 #include "include/panel_ili9806e_fwvga_video.h"
 #include "include/panel_hx8394d_qhd_video.h"
 #include "include/panel_hx8379c_fwvga_video.h"
+#include "include/panel_hx8379c_hvga_video.h"
 
 #define DISPLAY_MAX_PANEL_DETECTION 0
 #define ILI9806E_FWVGA_VIDEO_PANEL_POST_INIT_DELAY 68
@@ -69,6 +70,7 @@ enum {
 	ILI9806E_FWVGA_VIDEO_PANEL,
 	HX8394D_QHD_VIDEO_PANEL,
 	HX8379C_FWVGA_VIDEO_PANEL,
+	HX8379C_HVGA_VIDEO_PANEL,
 	UNKNOWN_PANEL
 };
 
@@ -85,6 +87,7 @@ static struct panel_list supp_panels[] = {
 	{"ili9806e_fwvga_video",ILI9806E_FWVGA_VIDEO_PANEL},
 	{"hx8394d_qhd_video", HX8394D_QHD_VIDEO_PANEL},
 	{"hx8379c_fwvga_video",HX8379C_FWVGA_VIDEO_PANEL},
+	{"hx8379c_hvga_video", HX8379C_HVGA_VIDEO_PANEL},
 };
 
 static uint32_t panel_id;
@@ -286,6 +289,27 @@ static int init_panel_data(struct panel_struct *panelstruct,
 		memcpy(phy_db->timing,
 					hx8379c_fwvga_video_timings, TIMING_SIZE);
 		pinfo->mipi.signature = HX8379C_FWVGA_VIDEO_SIGNATURE;
+		break;
+	case HX8379C_HVGA_VIDEO_PANEL:
+		panelstruct->paneldata    = &hx8379c_hvga_video_panel_data;
+		panelstruct->panelres     = &hx8379c_hvga_video_panel_res;
+		panelstruct->color        = &hx8379c_hvga_video_color;
+		panelstruct->videopanel   = &hx8379c_hvga_video_video_panel;
+		panelstruct->commandpanel = &hx8379c_hvga_video_command_panel;
+		panelstruct->state        = &hx8379c_hvga_video_state;
+		panelstruct->laneconfig   = &hx8379c_hvga_video_lane_config;
+		panelstruct->paneltiminginfo
+					= &hx8379c_hvga_video_timing_info;
+		panelstruct->panelresetseq
+					= &hx8379c_hvga_video_reset_seq;
+		panelstruct->backlightinfo = &hx8379c_hvga_video_backlight;
+		pinfo->mipi.panel_cmds
+					= hx8379c_hvga_video_on_command;
+		pinfo->mipi.num_of_panel_cmds
+					= HX8379C_HVGA_VIDEO_ON_COMMAND;
+		memcpy(phy_db->timing,
+					hx8379c_hvga_video_timings, TIMING_SIZE);
+		pinfo->mipi.signature = HX8379C_HVGA_VIDEO_SIGNATURE;
 		break;
 	case UNKNOWN_PANEL:
 	default:
