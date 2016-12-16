d81cd362dbdb28159bb2da84dbab440cebac7f0b
Nirmal Abraham <nabrah@codeaurora.org>
2016-12-16 15:14:40 +0530

    platform: msm_shared: Update LF copyright
    Update LF copyright to 2016 for smem.h.
    
    Change-Id: Idf56f68d9c0e7daee5cba051965ab942c9f441c2

--- platform/msm_shared/smem.h
+++ platform/msm_shared/smem.h
@@ -1,8 +1,8 @@
 /*
- * Copyright (c) 2009,2016 Google Inc.
+ * Copyright (c) 2009 Google Inc.
  * All rights reserved.
  *
- * Copyright (c) 2009-2015, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2009-2016, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
