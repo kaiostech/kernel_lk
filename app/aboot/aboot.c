fd2a7bdc83f4c73f00f19a26af8f541c62d1a4dc
Mayank Grover <groverm@codeaurora.org>
2017-01-18 20:00:40 +0530

    app: aboot: check for input image size for overflow
    Check for input image size for overflow in cmd_boot, incase verified
    boot is disabled.
    
    Change-Id: Ic663bf05ddf6670be20a4861f1521fcb3db4f002

--- app/aboot/aboot.c
+++ app/aboot/aboot.c
@@ -2251,6 +2251,7 @@ void cmd_boot(const char *arg, void *data, unsigned sz)
 	// Initialize boot state before trying to verify boot.img
 #if VERIFIED_BOOT
 		boot_verifier_init();
+#endif
 	/* Handle overflow if the input image size is greater than
 	 * boot image buffer can hold
 	 */
@@ -2259,7 +2260,6 @@ void cmd_boot(const char *arg, void *data, unsigned sz)
 		fastboot_fail("booimage: size is greater than boot image buffer can hold");
 		return;
 	}
-#endif
 
 	/* Verify the boot image
 	 * device & page_size are initialized in aboot_init
