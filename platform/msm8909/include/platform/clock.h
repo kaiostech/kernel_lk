2bb2a77d0480f8a8842962d595f541db7698a06b
Maria Yu <aiquny@codeaurora.org>
2017-02-24 16:37:32 +0800

    platform: msm8909: Add clock_config_blsp_i2c in header file
    Fix compile warning by adding clock_config_blsp_i2c in header file.
    
    Change-Id: I143292f4517fe3a9eeec64d77432e47fcc2d29c5

--- platform/msm8909/include/platform/clock.h
+++ platform/msm8909/include/platform/clock.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2014, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2014, 2017, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -74,4 +74,5 @@ void hsusb_clock_init(void);
 void clock_config_ce(uint8_t instance);
 void mdp_clock_init(void);
 void mdp_gdsc_ctrl(uint8_t enable);
+void clock_config_blsp_i2c(uint8_t blsp_id, uint8_t qup_id);
 #endif
